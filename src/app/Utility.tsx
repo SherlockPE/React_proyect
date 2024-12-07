2e3472b50611bbd1ad6c6f578bda822a27cc234b
SherlockPE <fabriciolopezreyes@gmail.com>
2024-12-07 23:42:14 +0100

    Mi primer proyecto en react

---
+++ src/app/Utility.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Utility({
+  name,
+  value,
+  icon,
+}: {
+  name: string;
+  value: string;
+  icon?: React.ReactNode;
+}) {
+  return (
+    <>
+      <div className="flex gap-32 p-2 items-center justify-between">
+        <div className="text-teal-200">{name}</div>
+        <div className="flex flex-row items-center gap-2">
+          {value}
+          {icon}
+        </div>
+      </div>
+    </>
+  );
+}
